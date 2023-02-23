fab1757449e40d4256d4eadfc0041842d496637a
Mahbub khan <mahbubk@live.com>
2023-02-23 17:45:57 +0000

    ok

--- src/main/java/com/example/todolist/TodoController.java
+++ src/main/java/com/example/todolist/TodoController.java
@@ -10,26 +10,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.todolist.ToDo;;
  
 
  
 @Controller
-@RequestMapping(path="/index")
+@RequestMapping(path="/")
 public class TodoController {
   @Autowired
   ToDoService service;
 
   public TodoController(ToDoService service) {
     this.service = service;
+    //this.service.saveAll(List.of(new ToDo(04,"Meeting with Boss","High","2023-03-22","Not Done")))
   }
  
-  @GetMapping
+  @GetMapping(path="/ToDoTbl")
   public String getAllToDos(Model model) 
   {
     List<ToDo> list = service.getAllToDos();
  
     model.addAttribute("ToDos", list);
-    return "index";
+    return "ToDoTbl";
   }
  
   @RequestMapping(path = {"/edit", "/edit/{id}"})
